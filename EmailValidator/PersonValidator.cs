e05b3f0ad7fd4b2518c8db0773fb4577aaadde2d
Donald Chow <donaldchow@Donalds-MacBook-Pro.local>
2016-02-22 20:59:03 +1100

    Initial Commit

---
+++ EmailValidator/PersonValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace EmailValidator
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(p => p.EmailAddress).EmailAddress()
+                .WithMessage("Not valid email address, please come again!");
+        }
+    }
+}
---
+++ EmailValidatorTests/PersonValidatorTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmailValidator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailValidator.Tests
+{
+    [TestClass]
+    public class PersonValidatorTests
+    {
+        [TestMethod]
+        public void PersonValidator_Test()
+        {
+            var person = new Person();
+            person.EmailAddress = "joe@hotmail.com";
+
+            var target = new PersonValidator();
+            var actual = target.Validate(person);
+
+            Assert.IsTrue(actual.IsValid);
+        }
+
+        [TestMethod]
+        public void PersonValidator_Bulk_Test()
+        {
+            var person = new Person();
+            var testcases = Create();
+            foreach (var testcase in testcases)
+            {
+                person.EmailAddress = testcase.Key;
+
+                var target = new PersonValidator();
+                var actual = target.Validate(person);
+
+                Assert.AreEqual(testcase.Value, actual.IsValid);
+            }   
+        }
+
+
+        private static IEnumerable<KeyValuePair<string, bool>> Create()
+        {
+            var testCases = new List<KeyValuePair<string,bool>>();
+            testCases.Add(new KeyValuePair<string, bool>("joe@outlook.com", true));
+            testCases.Add(new KeyValuePair<string, bool>("joe@outlook", false));
+            testCases.Add(new KeyValuePair<string, bool>("joe@outlook.mortgage", true));
+            testCases.Add(new KeyValuePair<string, bool>("\"j@oe\"@outlook.mortgage", true));
+            return testCases;
+        }
+
+
+    }
+}
